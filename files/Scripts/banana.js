1a89691267f0875e371616399b3c5070449ec6c8
BlackYukiDTD <67216376+BlackYukiDTD@users.noreply.github.com>
2021-04-19 14:00:10 +0900

    Update banana.js

--- files/Scripts/banana.js
+++ files/Scripts/banana.js
@@ -2,38 +2,34 @@
 Banana video unlock vip
 app download link : http://tinyurl.com/y57j6hjg
 
-XiaoXiaoYingShi unlock Vip
-QX:
-https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xxys.js
-Surge:
-http-response https:\/\/.*\.xiaoxiao.*\.com\/(vod\/reqplay\/|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xxys.js
+QX 1.0.0:
+^https?:\/\/.*\.(fuli|xiang(jiao|xiang))apps\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xjsp.js
 
-MITM = *.xiaoxiao*.com
+Surge 4.0：
+http-response https?:\/\/.*\.(fuli|xiang(jiao|xiang))apps\.com\/(ucp\/index|getGlobalData|.+\/reqplay\/) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xjsp.js
+
+Surge & QX MITM = ios.fuliapps.com, apple.fuliapps.com, ios.xiangjiaoapps.com, apple.xiangjiaoapps.com, *.xiangxiangapps.com
 */
 
-const path1 = "/ucp/index";
-const path2 = "/vod/reqplay/";
-const ad = 'getGlobalData';
-let obj = JSON.parse($response.body);
-
-if ($request.url.indexOf(path1) != -1){
-	obj.data.uinfo["down_daily_remainders"] = "666";
-	obj.data.uinfo["play_daily_remainders"] = "666";
-	obj.data.uinfo["curr_group"] = "5";
-	obj.data.user["isvip"] = "1";
-	obj.data.user["goldcoin"] = "666";
+var obj = JSON.parse($response.body);
+if ($request.url.indexOf("/ucp/index") != -1){
+  obj.data.uinfo.down_daily_remainders = "666";
+  obj.data.uinfo.play_daily_remainders = "666";
+  obj.data.uinfo["next_upgrade_need"] = "0";
+  obj.data.user.isvip = "1";
+  obj.data.user.gicon = "V5";
+  obj.data.user.gid = "5";
 }
-if ($request.url.indexOf(path2) != -1){
-	obj.retcode = "0";
-	obj.data.lastplayindex = "1";
-	if(obj.data.hasOwnProperty("httpurl_preview")){
-		var playurl = obj.data["httpurl_preview"];
-		obj.data["httpurl"] = playurl;
-	};
+if ($request.url.indexOf("/getGlobalData") != -1){
+  obj.data.app_launch_times_adshow = "0";
+  obj.data.adgroups = "";
+  obj.data.iOS_adgroups ="";
 }
-
-if ($request.url.indexOf(ad) != -1) {
-delete obj.data.adrows
-delete obj.data.adgroups
+if ($request.url.indexOf("/reqplay/") != -1){
+  obj.retcode = "0";
+  if(obj.data.hasOwnProperty("httpurl_preview")){
+  var playurl = obj.data["httpurl_preview"];
+  obj.data["httpurl"] = playurl;
+  };
 }
 $done({body: JSON.stringify(obj)});
