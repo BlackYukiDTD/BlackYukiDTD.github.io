609060d86aa74982599dd442c26b240c58137d4f
BlackYukiDTD <67216376+BlackYukiDTD@users.noreply.github.com>
2021-04-20 12:54:25 +0900

    Delete _sass/scripts directory

--- _sass/scripts/nhaccuatui.js
+++
@@ -1,9 +0,0 @@
-var obj = JSON.parse($response.body);
-let url = $request.url;
-var cons = "users/info";
-if(url.indexOf(cons) != -1)
-{
-obj.data.VIPExpire= "09/09/9999 00:00:00";
-obj.data.isVIP= true;
-}
-$done({body: JSON.stringify(obj)});
