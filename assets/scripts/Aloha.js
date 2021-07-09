a82841e27e7cd5290af486195dac39181dea212a
BlackYukiDTD <67216376+BlackYukiDTD@users.noreply.github.com>
2021-07-09 22:44:55 +0900

    Update Aloha.js

--- assets/scripts/Aloha.js
+++ assets/scripts/Aloha.js
@@ -1,15 +1,5 @@
 var obj = JSON.parse($response.body);
 
-obj = {
-  "profile": {
-    "id": 44722,
-    "email_verified": true,
-    "end_of_premium": 9999-12-30,
-    "_end_of_premium": 9999-12-30 23:59:00,
-    "email": "lil.love195@gmail.com",
-    "is_premium": true,
-    "token": "cfd404a67c1dc543f796cbe9e6a63b1d"
-  }
-};
+obj.profile.is_premium= true;
 
 $done({body: JSON.stringify(obj)});
